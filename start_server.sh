80c1a8a9886c62ecc3745f3036f16fe529762eaf
eloiseLBC <eloise.leblanc@hotmail.com>
2024-08-17 01:54:20 +0200

    add script starting server

---
+++ start_server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Démarrer le serveur Flask en arrière-plan
+echo "Démarrage du serveur Flask..."
+nohup python3 send_message.py &
+
+sleep 10
+
+# Démarrer LocalTunnel en arrière-plan
+echo "Démarrage de LocalTunnel..."
+nohup lt --port 3000 &
+# --subdomain votresousdomaine &
+
+# Remplacez "votresousdomaine" par le sous-domaine que vous souhaitez utiliser avec LocalTunnel.
+# Vous pouvez aussi omettre l'option --subdomain pour que LocalTunnel génère un sous-domaine aléatoire.
+
+echo "Le serveur Flask et LocalTunnel sont en cours d'exécution."
